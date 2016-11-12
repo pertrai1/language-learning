7cdb6ee113cb8d018bf154016de3f6860fb10d61
Rob Simpson <=>
2016-11-12 09:16:23 -0500

    update table to string

--- lua/util.lua
+++ lua/util.lua
@@ -8,7 +8,7 @@ function table_to_string(t)
 	local result = {}
 
 	for k, v in pairs(t) do
-		result[#result + 1] = k .. "; " .. v
+		result[#result + 1] = k .. ": " .. v
 	end
 
 	return table.concat(result, "\n")
