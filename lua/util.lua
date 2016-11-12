9a93562aa839e5ac0db6cd95ef0d8ce026a5b1ec
Rob Simpson <=>
2016-11-12 09:11:18 -0500

    adding table to string utility

--- lua/util.lua
+++ lua/util.lua
@@ -3,3 +3,14 @@ function print_table(t)
 		print(k .. "; " ..v)
 	end
 end
+
+function table_to_string(t)
+	local result = {}
+
+	for k, v in pairs(t) do
+		result[#result + 1] = k .. "; " .. v
+	end
+
+	return table.concat(result, "\n")
+end
+
