b418263144370776da94aeb8f81eeff948861f9f
Rob Simpson <rob.simpson@vbrick.com>
2016-12-08 04:49:33 -0500

    more js functions

--- javascript/good_parts/functions1.js
+++ javascript/good_parts/functions1.js
@@ -78,3 +78,89 @@ function curry2(fn, ...args1) {
 function curry3(fn, ...args) {
 	return (...args2) => fn(...args, ...args2);
 }
+
+function twice(binary) {
+    return function(a) {
+        return binary(a, a);
+    };
+}
+
+/**
+ * Write reverse, a function that reverses the arguments of a binary function
+ */
+
+function reverse(binary) {
+    return function(arg1, arg2) {
+        return binary(arg2, arg1);
+    };
+}
+
+/**
+ * Using ES6
+ */
+
+function reverse(func) {
+    return function (...args) {
+        return function func(...args.reverse());
+    };
+}
+
+/**
+ * Write a function composeu that takes two unary functions and returns a unary function that calls them both
+ */
+
+function composeu(func1, func2) {
+    return function(arg) {
+        return func2(func1(arg));
+    }
+}
+
+/**
+ * Write a function composeb that takes two binary functions and returns a function that calls them both
+ */
+
+function composeb(func1, func2) {
+    return function(a, b, c) {
+        return func2(func1(a, b), c);
+    };
+}
+
+/**
+ * Write a limit function that allows a binary function to be called a limited number of times
+ * I don't know how to do this one
+ */
+
+function limit(func, count) {
+    return function (a, b) {
+        if (count >= 1) {
+            count -= 1;
+            return func(a, b);
+        }
+
+        return undefined;
+    }
+}
+
+/**
+ * Write a from function that produces a generator that will produce a aeries of vaues
+ */
+
+function from(next, value) {
+    return function(a) {
+        if (value >= 1) {
+            value += 1;
+            return next(value);
+        }
+    }
+
+    return undefined;
+}
+
+// actual answer
+function from(start) {
+    return function () {
+        let next = start;
+        start += 1;
+        return next;
+    };
+}
