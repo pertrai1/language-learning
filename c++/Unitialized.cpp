09117c5acb89a226e456de4edfa5580bd15a9bdc
Rob Simpson <rob.simpson@vbrick.com>
2016-12-18 00:08:31 -0500

    Working with C++ variables

---
+++ c++/Unitialized.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    // define an integer named x
+    int x;
+
+    // print the value of x to the screen
+    std::cout << x;
+    // no that this is not going to give us what we are needing
+    return 0;
+}
---
+++ c++/cin.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a namber: ";
+    int x;
+    std::cin >> x;
+    std::cout << "You entered " << x << std::endl;
+    return 0;
+}
