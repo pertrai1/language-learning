d76408fe42a52de4046ddaed397ac2a0e7124521
Rob Simpson <rob.simpson@vbrick.com>
2016-12-19 23:54:33 -0500

    c++ enumerator example

---
+++ c++/enumerators/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+enum Monster
+{
+    MONSTER_ORCS,
+    MONSTER_GOBLINS,
+    MONSTER_TROLLS,
+    MONSTER_OGRES,
+    MONSTER_SKELETONS
+};
+
+Monster monster = MONSTER_TROLLS;
