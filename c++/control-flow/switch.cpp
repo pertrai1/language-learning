6b173f6da3f1a210637a0654c1a7e9206df9bc75
Rob Simpson <rob.simpson@vbrick.com>
2016-12-22 14:47:59 -0500

    Switch control flow example

---
+++ c++/control-flow/switch.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int calculate(int x, int y, char op)
+{
+	switch(op)
+	{
+		case '+':
+			return x + y;
+			break;
+		case '-':
+			return x - y;
+			break;
+		case '*':
+			return x * y;
+			break;
+		case '/':
+			return x / y;
+			break;
+		case '%':
+			return x % y;
+			break;
+		default:
+			std::cout << "calculate(): Unhandled case\n";
+			return 0;
+	}
+}
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+	int x;
+	std::cin >> x;
+
+	std::cout << "Enter another integer: ";
+	int y;
+	std::cin >> y;
+
+	std::cout << "Enter a mathematical operator: ";
+	char op;
+	std::cin >> op;
+
+	std::cout << x << " " << op << " " << y << " is " << calculate(x, y, op) << "\n";
+	return 0;
+}
